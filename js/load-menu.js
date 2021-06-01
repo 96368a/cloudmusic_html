d32a859c8ff06162a05114804cbc9fb76059246f
木末 <ncxxg@qq.com>
2021-06-01 08:31:38 +0800

    进度0.002% 😪

--- js/load-menu.js
+++ js/load-menu.js
@@ -1,8 +1,16 @@
+
+function load_songlist(id){
+    _url="https://163.233c.cn/playlist/detail?id="
+    console.log
+    return $.getJSON(_url+id);
+}
+
 $(function(){
     let userid=607728498;
 var menu=$.getJSON("https://163.233c.cn/user/playlist?uid="+userid,function(menu){
     menu.playlist.forEach(e => {
         let menu_list=$("<li><a href=javascript:; class=\"menu_song\">"+e.name+"</a></li>");
+        menu_list.on('click',load_songlist(e.id))
         if(e.userId==userid)
         $("#favorite_list").before(menu_list)
         else
